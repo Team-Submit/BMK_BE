5fa2a8c1a7b08f5e329775cc30153d3a158e749b
양희범 <sinbla78@gmail.com>
2024-02-17 16:31:06 +0900

    ADD: 게시글 상세보기

--- src/controller/group.ts
+++ src/controller/group.ts
@@ -1,7 +1,7 @@
 import { AppDataSoure } from "../models/dataSource";
-import { Group } from "../models/group";
+import { Group } from '../models/group';
 
-export const userRepository = AppDataSoure.getRepository(Group);
+export const groupRepository = AppDataSoure.getRepository(Group);
 
 exports.group_post = async (req, res) => {
     const { title, categori, price, content, image, personnelAll, transactionDate, time} = req.body;
@@ -16,7 +16,7 @@ exports.group_post = async (req, res) => {
     newGroupPost.transactionDate = transactionDate;
     newGroupPost.time = time;
 
-    const group = await userRepository.save(newGroupPost);
+    const group = await groupRepository.save(newGroupPost);
     return res.status(200).json(group);
 };
 
@@ -38,7 +38,7 @@ exports.group_edit = async (req, res) => {
         time
     };
 
-    const group = await userRepository.update(id, update);
+    const group = await groupRepository.update(id, update);
     return res.status(200).json(group);
 };
 
@@ -49,6 +49,18 @@ exports.group_del = async (req, res) => {
         return res.status(400).json({ message: 'id가 올바르지 않습니다.'});
     }
 
-    const group = await userRepository.delete(id);
+    const group = await groupRepository.delete(id);
     return res.status(200).json(group);
+};
+
+exports.group_details = async (req, res) => {
+    const groupId = req.params.groupId;
+
+    const group = await groupRepository.findOne(groupId);
+
+    if (!group) {
+        return res.status(404).json({ message: '글을 찾지 못했습니다.' });
+    }
+
+    res.json(group);
 };
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,12 @@
 import express, { Application, Request, Response } from 'express'
 
 const router = express.Router();
-const Used = require('controller/used');
 const Group = require('controller/group');
 
-router.post('/used/post', Used.used_post);
-router.patch('/used/boards', Used.used_edit);
-router.delete('/used/boards', Used.used_del);
-
-router.post('/group/boards', Group.used_post);
-router.patch('/group/boards', Group.used_edit);
-router.delete('/group/boards', Group.used_del);
+router.post('/group/boards', Group.group_post);
+router.patch('/group/boards', Group.group_edit);
+router.delete('/group/boards', Group.group_del);
+router.delete('/group/boards/:groupId', Group.group_details);
 
 module.exports = router;
 
