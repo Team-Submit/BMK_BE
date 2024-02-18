1fa9721498f63445b25fd41402736b46b9c31c05
Jin hyeon <kangjin627@gmail.com>
2024-02-18 21:20:59 +0900

    ADD: redis 설정

--- src/app.ts
+++ src/app.ts
@@ -3,26 +3,33 @@ import express, { Application, Request, Response } from 'express'
 import { AppDataSoure } from './models/dataSource';
 import cors from 'cors';
 import router from './router/index';
+import redisCli from '../redis';
 
 configDotenv();
 const app: Application = express();
 const port: number = Number(process.env.PORT) || 8000;
 
+// db설정
 AppDataSoure.initialize()
     .then(() => { console.log(`DB has initted`) })
     .catch((err) => { console.error(err) });
 
+// cors 설정
 app.use(cors({
     origin: '*',
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     credentials: true
 }))
 
+// 기본 설정
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/', router);
 
-app.listen(port, function () {
+// redis 설정
+
+app.listen(port, async () => {
+    await redisCli.connect()
     console.log(`App is listening on port ${port} !`)
 })
