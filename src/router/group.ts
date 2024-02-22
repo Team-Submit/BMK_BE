4a388ad8c9ae5fcf12a696bf865c06fe5451afeb
양희범 <sinbla78@gmail.com>
2024-02-22 09:27:20 +0900

    ADD: 공구게시글 찜 추가

--- src/router/group.ts
+++ src/router/group.ts
@@ -9,5 +9,6 @@ router.delete('/group/boards', Group.group_del);
 router.delete('/group/boards/:groupId', Group.group_details);
 router.get('/group/boards', Group.group_list);
 router.get('/group/search', Group.group_search);
+router.patch('/picks', Group.group_picks);
 
 export default router;
