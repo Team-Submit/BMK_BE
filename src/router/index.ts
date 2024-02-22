dd95d4f13df028364f702f1153d69062ecbd0565
Jin hyeon <kangjin627@gmail.com>
2024-02-22 17:09:14 +0900

    ADD: 채팅 구현

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import express, { Router } from "express";
 import user from './user';
 import auth from './auth';
+import chat from './chat'
 
 const router: Router = express.Router();
 
 router.use('/members', user)
 router.use('/auth', auth)
+router.use('/chatting', chat)
 
 export default router;
