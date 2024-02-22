416724fe07e778998f3641947332963e2b03e895
Jin hyeon <kangjin627@gmail.com>
2024-02-22 16:58:03 +0900

    Revert "Merge branch 'feature/chat' into develop"
    This reverts commit 0a2efa2fae3910b082a638cd22627a38af5a8636, reversing
    changes made to 07596a8cae8b145dc20f9ae5b2f584192be4613e.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,10 @@
 import express, { Router } from "express";
 import user from './user';
 import auth from './auth';
-import chat from './chat'
 
 const router: Router = express.Router();
 
 router.use('/members', user)
 router.use('/auth', auth)
-router.use('/chatting', chat)
 
 export default router;
