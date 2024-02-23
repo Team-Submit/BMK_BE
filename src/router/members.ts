120f6c1720086e5068530984cbebe8a22d775c2e
Jin hyeon <kangjin627@gmail.com>
2024-02-23 18:53:11 +0900

    ADD: 다른 회원정보 불러오기, 정보 수정, 계정 탈퇴

--- src/router/members.ts
+++ src/router/members.ts
@@ -1,9 +1,12 @@
 import express, { Router } from "express";
-import { getUser } from "../controller/user";
+import { delacc, getAnotherUser, getUser, info } from "../controller/user";
 import { validationAccess } from "../middleware/jwt";
 
 const router: Router = express.Router();
 
 router.get('/', validationAccess, getUser)
+router.get('/:studentId', validationAccess, getAnotherUser)
+router.patch('/', validationAccess, info)
+router.delete("/", validationAccess, delacc)
 
 export default router;
