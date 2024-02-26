58ee48f62b94459a93836314785bccaf6cd4b97c
양희범 <sinbla78@gmail.com>
2024-02-26 13:22:04 +0900

    FIX: 에러처리 추가

--- src/controller/group.ts
+++ src/controller/group.ts
@@ -4,66 +4,87 @@ import Group from '../models/group.entity';
 export const groupRepository = AppDataSoure.getRepository(Group);
 
 const group_post = async (req, res) => {
-    const { title, category, price, content, image, personnelAll, transactionDate, time} = req.body;
+    try {
+        const { title, category, price, content, image, personnelAll, transactionDate, time} = req.body;
 
-    const newGroupPost = new Group();
-    newGroupPost.title = title;
-    newGroupPost.category = category;
-    newGroupPost.price = price;
-    newGroupPost.content = content;
-    newGroupPost.image = image;
-    newGroupPost.personnelAll = personnelAll;
-    newGroupPost.transactionDate = transactionDate;
-    newGroupPost.time = time;
+        const newGroupPost = new Group();
+        newGroupPost.title = title;
+        newGroupPost.category = category;
+        newGroupPost.price = price;
+        newGroupPost.content = content;
+        newGroupPost.image = image;
+        newGroupPost.personnelAll = personnelAll;
+        newGroupPost.transactionDate = transactionDate;
+        newGroupPost.time = time;
 
-    const group = await groupRepository.save(newGroupPost);
-    return res.status(200).json(group);
+        const group = await groupRepository.save(newGroupPost);
+        return res.status(200).json(group);
+    } catch (error) {
+        console.error('Error while creating used list:', error);
+        res.status(500).json({ message: '서버에서 글을 생성하는 중에 오류가 발생했습니다.' });
+    }
 };
 
 const group_edit = async (req, res) => {
-     const {title, category, price, content, image, personnelAll, transactionDate, time} = req.body;
-     const id = req.params.id;
+    try {
+        const {title, category, price, content, image, personnelAll, transactionDate, time} = req.body;
+        const id = req.params.id;
 
-    if (!id) {
-        return res.status(400).json({ message: 'id가 올바르지 않습니다.'});
+        if (!id) {
+            return res.status(400).json({ message: 'id가 올바르지 않습니다.'});
+        }
+
+        const update = {
+            title,
+            category,
+            price,
+            content,
+            image,
+            personnelAll,
+            transactionDate,
+            time
+        };
+
+        const group = await groupRepository.update(id, update);
+        return res.status(200).json(group);
+    } catch(error) {
+        console.error('Error while editing used list:', error);
+        res.status(500).json({ message: '서버에서 글을 수정하는 중에 오류가 발생했습니다.' });
     }
-
-    const update = {
-        title,
-        category,
-        price,
-        content,
-        image,
-        personnelAll,
-        transactionDate,
-        time
-    };
-
-    const group = await groupRepository.update(id, update);
-    return res.status(200).json(group);
 };
 
 const group_del = async (req, res) => {
-    const id = req.params.id;
+    try {
+        const id = req.params.id;
 
-    if (!id) {
-        return res.status(400).json({ message: 'id가 올바르지 않습니다.'});
+        if (!id) {
+            return res.status(400).json({ message: 'id가 올바르지 않습니다.'});
+        }
+
+        const group = await groupRepository.delete(id);
+        return res.status(200).json(group);
+    } catch (error) {
+        console.error('Error while deleting used list:', error);
+        res.status(500).json({ message: '서버에서 글을 삭제하는 중에 오류가 발생했습니다.' });
     }
-
-    const group = await groupRepository.delete(id);
-    return res.status(200).json(group);
 };
 
 const group_details = async (req, res) => {
-    const groupId = req.params.groupId;
+    try {
+        const groupId = req.params.groupId;
 
-    const group = await groupRepository.findOne({where: {group_id: groupId}});
+        const group = await groupRepository.findOne({where: {group_id: groupId}});
 
-    if (!group) {
-        return res.status(404).json({ message: '글을 찾지 못했습니다.' });
+        if (!group) {
+            return res.status(404).json({ message: '글을 찾지 못했습니다.' });
+        }
+
+        res.json(group);
+    } catch (error) {
+        console.error('Error while bring used list:', error);
+        res.status(500).json({ message: '서버에서 글을 가져오는 중에 오류가 발생했습니다.' });
     }
-
-    res.json(group);
+    
 };
 
 const group_list = async (req, res) => {
@@ -77,13 +98,13 @@ const group_list = async (req, res) => {
 }
 
 const group_search = async (req, res) => {
-    const { title } = req.query;
-
-    if (!title) {
-        return res.status(400).json({ message: '제목을 입력해주세요.' });
-    }
-
     try {
+        const { title } = req.query;
+
+        if (!title) {
+            return res.status(400).json({ message: '제목을 입력해주세요.' });
+        }
+
         const searchResult = await groupRepository.find({ where: { title: title } });
 
         if (searchResult.length === 0) {
